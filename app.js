94ba02918d3cd54c2ed77ea70825f25a846d80c1
장도훈 <jehgns1@gmail.com>
2024-04-09 14:26:02 +0900

    기본 틀 만들고 contact에서 정보 입력하면 다시 보이는것까지! (body-parser 사용!)

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
 const express = require('express')
+const ejs = require('ejs') 
 const app = express()
 const port = 3000
+var bodyParser = require('body-parser')
 
-//라우터 
-app.get('/', (req,res) => {
-  res.send("<h1>hello world!</h1> ")  // 사용자에게 보여주는 부분 
+
+app.set('view engine','ejs')
+app.set('views','./views')
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// 기본 페이지!!
+app.get('/', (req, res) => {
+  res.render('index')   // ./views/index.ejs  
 })
+
+app.get('/profile', (req,res) => {
+  res.render("profile")  
+})
+
+app.get('/map', (req,res) => {
+  res.render("map")  
+})+
+
+app.get('/contact', (req,res) => {
+  res.render("contact")  
+})
+
+app.post('/contactProc', (req, res) => {
+  const name = req.body.name; 
+  const phone = req.body.phone; 
+  const email = req.body.email; 
+  const memo = req.body.memo; 
+
+  var a = ` ${name} ${phone} ${email} ${memo} `
+  res.send(a);
+  // var sql = `insert into contact(name,phone,email,memo,regdate)
+  // values(?,?,?,?,now() )`
+   
+  // var values = [name,phone,email,memo]; 
+
+  // connection.query(sql, values, function (err, result){
+  //     if(err) throw err; 
+  //     console.log('자료 1개를 삽입하였습니다.');
+  //     res.send("<script> alert('문의사항이 등록되었습니다.'); location.href='/';</script>"); 
+  // })
+
+})
+
+
+
 
 app.listen(port, () =>{
   console.log(`서버가 실행되었습니다. 접속주소 : http://localhost:${port}`)
